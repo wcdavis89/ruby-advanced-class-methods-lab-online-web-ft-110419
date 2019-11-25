fe6a41cac1efc3e04d995bd49d69081c2b1379f7
Whittney Davis <wcdavis89@yahoo.com>
2019-11-25 02:30:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,9 +5,36 @@ class Song
   def self.all
     @@all
   end
+  def self.create
+    song = self.new
+    song.name = name
+    @@all << song
+    song
+  end
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+  def self.create_by_name(name)
+     song = self.new
+    song.name = name
+    @@all << song
+    song
+  end
+  def self.find_by_name(name)
+    @@all.find{|song| song.name == name}
+  end
+  def self.find_or_create_by_name(name)
+    
+      @@all.find{|person| person.name == name}
+      
+  end
 
   def save
     self.class.all << self
   end
-
+  def self.destroy_all
+    self.all.clear
+  end
 end
