3b10c364e0adbb592476260a4c4f2b77ca3761ff
Whittney Davis <wcdavis89@yahoo.com>
2019-11-25 02:44:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,7 +28,7 @@ class Song
   def self.find_or_create_by_name(name)
       if @@all.find_by_name == true 
         puts song
-      else self
+      else self.create_by_name(name)
   end
       
   end
